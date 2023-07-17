7a1236892c9fe49f9057adc3960a45ea6360cd2e
bea-menezes <beatrizprs.menezes@gmail.com>
2023-07-17 11:17:43 -0400

    Aula 01

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
 
   return (
     <>
-      <div>
-        Item Card
+      <div className='card'>
+        <h1>Titulo do item</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/en/6/6e/Link_to_the_Past_Zelda.png"/>
       </div>
     </>
   )
